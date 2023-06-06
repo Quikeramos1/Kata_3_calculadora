8c927e0a84934ed6fa58a739c93b60ed02778ab0
Quikeramos1 <133090561+Quikeramos1@users.noreply.github.com>
2023-06-06 13:41:42 +0200

    clase ventana creada

---
+++ hello_con_clases.py
@@ -0,0 +1,24 @@
+import tkinter as tk
+
+class Ventana(tk.Tk):
+
+    def __init__(self):
+        super(). __init__()
+        self.title("Mi primera pantalla tkinter")
+        self.geometry("800x600+400+200")
+        self.label = tk.Label(self, text="", bd=2,relief=tk.RAISED, width=50)
+        self.label.pack()
+        self.valor_nombre = tk.StringVar()
+        nombre = tk.Entry(self, textvariable=self.valor_nombre)
+        nombre.pack()
+        boton = tk.Button(self, text ="Púlsame", command= self.imprimir_saludo)
+        boton.pack()
+        
+    def imprimir_saludo(self):
+        self.label.config(text=f"Hola, {self.valor_nombre.get()}")
+    
+
+
+#ahora vamos a instanciarla para ver que la clase funciona
+
+Ventana().mainloop()
