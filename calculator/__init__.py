e737ccd11e257f07390558eb2aeb0d8e6107009a
Quikeramos1 <133090561+Quikeramos1@users.noreply.github.com>
2023-06-07 21:37:02 +0200

    Varios botones y 2 pantallas con par e impar

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from .controls import Display
+from .controls import Display, CalcButton
 
 WIDTH = 272
 HEIGHT = 300
@@ -10,9 +10,28 @@ class Calculator(tk.Tk):
         self.title("Calculadora")
         self.geometry(f"{WIDTH}x{HEIGHT}")
 
-        display = Display(self)
-        display.pack()
+        self.display = Display(self)
+        self.display.pack()
 
-        display.typing("198.368.681.654")
+        self.display.typing("198.368.681.654")
 
+        CalcButton(self, self.clic1, "1").pack()
+        CalcButton(self, text="2", tiny_wire=self.clic2).pack()
+        CalcButton(self, text="3", tiny_wire=self.clic3).pack()
+        CalcButton(self, text="4", tiny_wire=self.clic4).pack()
 
+        self.display2 = Display(self)
+        self.display2.pack()
+        
+
+    def clic1(self):
+        self.display.typing("1")
+
+    def clic2(self):
+        self.display2.typing("2")  
+
+    def clic3(self):
+        self.display.typing("3")  
+
+    def clic4(self):
+        self.display2.typing("4")  
--- calculator/controls.py
+++ calculator/controls.py
@@ -15,4 +15,14 @@ class Display(tk.Frame):
         self.label.config(text=text)
 
     
+class CalcButton(tk.Frame):
+    def __init__(self, location, tiny_wire, text):
+        super().__init__(location, width=68, height=50)
+        self.pack_propagate(False)
+        self.button = tk.Button(self, text=text, command=tiny_wire)
+        self.button.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
+    def clic(self):
+        Display.typing(self,"Total")
+
+ 
