c09c2017f4fc5c7c2424aede60948f6fe3165245
Quikeramos1 <133090561+Quikeramos1@users.noreply.github.com>
2023-06-12 21:03:36 +0200

    Funcionando casi todo, mostrando errores, falta ordenar los operadores

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from .controls import Display, CalcButton, KeyBoard
-from cromannumbers import RomanNumber
+from cromannumbers import RomanNumber, RomanNumberError
 
 WIDTH = 272
 HEIGHT = 300
@@ -10,7 +10,6 @@ operaciones = {
     "-": lambda a, b: a - b,
     "x": lambda a, b: a * b,
     "÷": lambda a, b: a // b
-
 }
 
 class Calculator(tk.Tk):
@@ -18,46 +17,57 @@ class Calculator(tk.Tk):
         super().__init__()
         self.title("Calculadora")
         self.geometry(f"{WIDTH}x{HEIGHT}")
-        self.op1 = None
-        self.op2 = None
-        self.operation = None
-        self.value = ""
-        self.resultado = None
+        self.reset()
 
         self.display = Display(self)
         self.display.pack()
-        
+
         KeyBoard(self, self.clic).pack()
 
-        self.display.typing("")
-        
+    def reset(self):
+        self.value = ""
+        self.op1 = self.op2 = self.operation = None
+        self.equal = False
 
     def clic(self, tecla):
-        if tecla == "AC":
-            self.value = ""
-            self.op1 = self.op2 = self.operation = None
-        elif tecla in operaciones:
-            self.op1 = RomanNumber(self.value)
-            self.operation = tecla
-        elif tecla == "=":
-            self.op2 = RomanNumber(self.value)
-            resultado = operaciones[self.operation](self.op1, self.op2)
-            self.value = resultado.simbolo
+        try:
+            if tecla == "AC":
+                self.reset()
+            elif tecla in operaciones:
+                if self.operation:
+                    if self.equal:
+                        resultado = RomanNumber(self.value)
+                    else:
+                        self.op2 = RomanNumber(self.value)
+                        resultado = operaciones[self.operation](self.op1, self.op2)
+                    self.op1 = resultado
+                    self.op2 = None
+                    self.value = resultado.simbolo
+                else:
+                    self.op1 = RomanNumber(self.value)
+                self.operation = tecla
+            elif tecla == "=":
+                if self.equal:
+                    resultado = operaciones[self.operation](RomanNumber(self.value), self.op2)
+                else:
+                    self.op2 = RomanNumber(self.value)
+                    resultado = operaciones[self.operation](self.op1, self.op2)
+                self.value = resultado.simbolo
+            else:
+                if self.equal:
+                    self.reset()
 
+                if self.operation and self.op2 is None:
+                    self.value = ""
+                    self.op2 = ""
+                
+                self.value += tecla
             
-        
+            self.display.typing(self.value)
 
-        else:  
-            if self.operation is not None and self.op2 is None:
-                self.value = ""
-                self.op2 = ""
-            self.value += tecla
-
-          
-        self.display.typing(self.value)
+            self.equal = (tecla == "=")
+        except RomanNumberError:
+            self.value = "E"
+            self.display.typing(self.value)
+            self.reset()
         
-
-        
-
-        
-    
--- calculator/controls.py
+++ calculator/controls.py
@@ -2,19 +2,18 @@ import tkinter as tk
 from tkinter.font import Font
 
 
-
 class Display(tk.Frame):
     def __init__(self, location):
         super().__init__(location, width=272, height=50)
         self.pack_propagate(False)
-        self.label = tk.Label(self, background="#000000", text="", foreground="white",
-                              anchor=tk.E, padx=10, pady=10, font=Font(family="Arial", size="30"))
+        self.label = tk.Label(self, background="#000000", foreground="#FFFFFF",
+                              anchor=tk.E, padx=8, 
+                              font=Font(family="Arial", size="40"))
         self.label.pack(side=tk.TOP, fill=tk.BOTH, expand=True)
 
     def typing(self, text):
         self.label.config(text=text)
 
-    
 class CalcButton(tk.Frame):
     def __init__(self, location, tiny_wire, text):
         super().__init__(location, width=68, height=50)
@@ -26,17 +25,15 @@ class CalcButton(tk.Frame):
 
     def pressed(self):
         self.tiny_wire(self.text)
-        
-
 
 class KeyBoard(tk.Frame):
     def __init__(self, location, tiny_wire):
         super().__init__(location, width=272, height=250)
-        CalcButton(self, tiny_wire, "AC").grid(column=0, row=0, columnspan=3, sticky="we")
+        CalcButton(self, tiny_wire, "AC").grid(column=0, row=0, columnspan=3, sticky="WENS")
         CalcButton(self, tiny_wire, "÷").grid(column=3, row=0)
         CalcButton(self, tiny_wire, "C").grid(column=0, row=1)
         CalcButton(self, tiny_wire, "D").grid(column=1, row=1)
-        CalcButton(self, tiny_wire, "M").grid(column=2, row=1, rowspan=3, sticky="ns")
+        CalcButton(self, tiny_wire, "M").grid(column=2, row=1, rowspan=3, sticky="NS")
         CalcButton(self, tiny_wire, "x").grid(column=3, row=1)
         CalcButton(self, tiny_wire, "X").grid(column=0, row=2)
         CalcButton(self, tiny_wire, "L").grid(column=1, row=2)
@@ -44,7 +41,14 @@ class KeyBoard(tk.Frame):
         CalcButton(self, tiny_wire, "I").grid(column=0, row=3)
         CalcButton(self, tiny_wire, "V").grid(column=1, row=3)
         CalcButton(self, tiny_wire, "+").grid(column=3, row=3)
-        CalcButton(self, tiny_wire, "=").grid(column=0, row=4, columnspan=4, sticky="we")
+        CalcButton(self, tiny_wire, "=").grid(column=0, row=4, columnspan=4, sticky="NSEW")
 
 
- 
+
+
+
+
+
+
+
+
